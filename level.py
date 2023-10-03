8ab5ca4cec80810177543770fa94b321526ebdd4
Rolf Seewaldt <rolf.seewaldt@gmail.com>
2023-10-03 19:54:58 +0200

    Added Player functions

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__(self):
@@ -10,7 +11,7 @@ class Level:
         
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
         
         # sprinte setup
         self.create_map()
@@ -21,10 +22,12 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == "x":
-                    Tile((x,y), [self.visible_sprites, self.obstacles_sprites])
+                    Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
                 if col == "p":
-                    Player((x,y), [self.visible_sprites])
+                    self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
                      
     def run(self):
         # update and draw the game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
